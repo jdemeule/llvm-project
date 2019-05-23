c48b686cfdd122114a3f41011b20edef7a8f4f12
Jérémy Demeule <jeremy.demeule@gmail.com>
2019-05-23 16:45:58 +0200

    Add tests

--- clang-tools-extra/test/clang-tidy/modernize-use-ranges.cpp
+++ clang-tools-extra/test/clang-tidy/modernize-use-ranges.cpp
@@ -1,14 +1,75 @@
 // RUN: %check_clang_tidy %s modernize-use-ranges %t
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [modernize-use-ranges]
+// #include <ranges>
+namespace std {
+template <class T>
+T &&declval() noexcept;
+template <class _Ip, class _Op>
+_Op copy(_Ip first, _Ip last, _Op out) { return out; }
+template <class C>
+typename C::const_iterator begin(const C &c) { return c.begin(); }
+template <class C>
+typename C::const_iterator end(const C &c) { return c.end(); }
+template <class T>
+class vector {
+public:
+  using const_iterator = const T *;
+  const_iterator begin() const {
+    return {};
+  }
+  const_iterator end() const {
+    return {};
+  };
+};
+struct back_insert_iterator {
+};
+template <class C>
+back_insert_iterator back_inserter(C &) {
+  return {};
+}
+namespace ranges {
+template <class T>
+using iterator_t = decltype(std::begin(declval<T &>()));
+template <class R>
+using safe_iterator_t = iterator_t<R>;
+template <class I, class O>
+struct copy_result {
+  I in;
+  O out;
+};
+template <class I, class S, class O>
+constexpr copy_result<I, O> copy(I first, S last, O result) {
+  return {last, result};
+}
+template <class R, class O>
+constexpr copy_result<safe_iterator_t<R>, O> copy(R &&r, O result) {
+  return {std::end(r), result};
+}
+} // namespace ranges
+} // namespace std
 
-// FIXME: Verify the applied fix.
-//   * Make the CHECK patterns specific enough and try to make verified lines
-//     unique to avoid incorrect matches.
-//   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
+void std_copy_1() {
+  std::vector<int> v1;
+  std::vector<int> out;
+  std::copy(v1.begin(), v1.end(), std::back_inserter(out));
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Consider to replace 'std::copy' by 'std::ranges::copy' [modernize-use-ranges]
+}
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
+void std_copy_2() {
+  std::vector<int> v2;
+  std::vector<int> out;
+  std::copy(std::begin(v2), std::end(v2), std::back_inserter(out));
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Consider to replace 'std::copy' by 'std::ranges::copy' [modernize-use-ranges]
+}
+
+void range_1() {
+  std::vector<int> vs;
+  std::vector<int> out;
+  std::ranges::copy(vs.begin(), vs.end(), std::back_inserter(out));
+}
+
+void range_2() {
+  std::vector<int> vs;
+  std::vector<int> out;
+  std::ranges::copy(vs, std::back_inserter(out));
+}
